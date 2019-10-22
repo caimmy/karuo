f0eb623308ca889417ec92cb888b534b0c7e53c6
caimiao <caimiao@kingsoft.com>
2019-10-22 18:10:51 +0800

    modify logger on timed rotating.

--- karuo/helpers/logger_helper.py
+++ karuo/helpers/logger_helper.py
@@ -13,26 +13,36 @@ Change Activity:
 
 import logging
 import logging.handlers
+import os
 
-def getTimedRotatingLogger(filename, when="d", interval=1, backupCount=30):
-    # logging    初始化工作
-    gen_logger = logging.getLogger("zjlogger")
-    gen_logger.setLevel(logging.DEBUG)
+class LoggerTimedRotating(object):
+    _instances = {}
 
-    # 添加TimedRotatingFileHandler
-    # 定义一个1秒换一次log文件的handler
-    # 保留3个旧log文件
-    rf_handler = logging.handlers.TimedRotatingFileHandler(filename=filename, when=when, interval=interval,
-                                                           backupCount=backupCount)
-    rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+    @staticmethod
+    def getInstance(filename, when='d', interval=1, backupCount=30, logger='karuo', level=logging.DEBUG) -> logging.Logger:
+        cls = __class__
+        if logger not in cls._instances:
+            _log_path = os.path.abspath(os.path.dirname(filename))
+            if not os.path.isdir(_log_path):
+                os.mkdir(_log_path)
+            if os.path.isdir(_log_path):
+                gen_logger = logging.getLogger(logger)
+                gen_logger.setLevel(level)
 
-    # 在控制台打印日志
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+                rf_handler = logging.handlers.TimedRotatingFileHandler(filename=filename, when=when, interval=interval,
+                                                                       backupCount=backupCount)
+                rf_handler.setFormatter(
+                    logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
 
-    gen_logger.addHandler(rf_handler)
-    gen_logger.addHandler(handler)
+                # 在控制台打印日志
+                handler = logging.StreamHandler()
+                handler.setLevel(level)
+                handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 
+                gen_logger.addHandler(rf_handler)
+                gen_logger.addHandler(handler)
 
-    return gen_logger
+                cls._instances.setdefault(logger, gen_logger)
+            else:
+                raise IOError("logger file not exists")
+        return cls._instances.get(logger)
